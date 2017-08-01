edea1ae93b6c8acb28615c179e5b9f00ed222485
Ni Tongyang <ntyaiqingr@gmail.com>
2017-08-01 13:54:04 -0400

    css selector with class or id & fade in out and to

---
+++ js/test.js
@@ -0,0 +1,8 @@
+$(function() {
+  //dot for a class name
+  //# for a speicify id name
+  $(".red-box").fadeOut(2000);
+  $(".green-box").fadeOut(2000);
+  $(".red-box").fadeIn(1000);
+  $(".red-box").fadeTo(1000,0.5);
+});
